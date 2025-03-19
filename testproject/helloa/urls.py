25e24469be0afbe2d51a69b60bcd7a63351e48f7
Enkh0824 <amaraaa0824@gmail.com>
2025-03-19 09:31:57 +0800

    template

--- testproject/helloa/urls.py
+++ testproject/helloa/urls.py
@@ -3,5 +3,8 @@ from .import views
 urlpatterns = [
     path ("",views.index,name="index"),
     path ("w",views.wrong,name="wrong"),
-    path ("u",views.uidalt,name="uidalt")
+    path ("u",views.uidalt,name="uidalt"),
+    path("<str:language>",views.greeting,name="greeting"),
+
+
 ]
--- testproject/helloa/views.py
+++ testproject/helloa/views.py
@@ -2,22 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("sn bnu")
+    # return HttpResponse("sn bnu")
+    return render(request, "helloa/index.html", {"value": "sain baina uu" , "type":"mongol", "a":78, "items":["hjkl","ghjkl;","ertye"]})
 def wrong(request):
     return HttpResponse ("buruu site")
 def uidalt(request):
     return HttpResponse ("yu bnda")
+
 def greeting(request,language):
-    if "mongolian" in language
-    return HttpResponse("amar bnu")
+    if "mongolian" in language: 
+        return HttpResponse("amar bnu")
     elif "chinise" in language:
         return HttpResponse("ni hao")
     elif "russian" in language:
-        return HttpResponse(snovi gdom)
+        return HttpResponse("snovi gdom")
     else: 
         return HttpResponse("hello")
-urlpatterns = [
-    ... 
-    path("str:language>",views.greeting,name="greeting"),
-    ... 
-]
+
